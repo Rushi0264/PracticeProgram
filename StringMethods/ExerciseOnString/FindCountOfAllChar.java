842a57d73f52cab475527bf6bfe82e99953ea903
Rushi0264 <rushikeshchepte2@gmail.com>
2025-11-08 11:01:01 +0530

    add

---
+++ StringMethods/ExerciseOnString/FindCountOfAllChar.java
@@ -0,0 +1,23 @@
+package StringMethods.ExerciseOnString;
+
+public class FindCountOfAllChar {
+    public static void main(String[] args) {
+        String str = "Java Programming";
+
+        str=str.toLowerCase();
+        str=str.replace(" ","");
+
+        int[] freq = new int[256];
+
+        for (int i=0; i<str.length(); i++){
+            freq[str.charAt(i)]++;
+        }
+
+        System.out.println("Frequency of char : ");
+        for (int i=0; i<freq.length;i++){
+            if (freq[i] > 0){
+                System.out.println((char) i +" = "+freq[i]);
+            }
+        }
+    }
+}
